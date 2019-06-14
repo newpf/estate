4127f66c92cd03953c20edf9454dd294302d84e5
小飞 <1967447613@qq.com>
2019-06-14 17:29:56 +0800

    first commit

---
+++ yun/js/sql/data.js
@@ -0,0 +1,83 @@
+var data = {
+    "0": {
+        "id": 0,
+        "pid": -1,
+        "title": "莱唔智能科技",
+        "type": "file",
+        "checked":false
+    },
+    "1": {
+        "id": 1,
+        "pid": 0,
+        "title": "访客",
+        "type": "file",
+        "checked":false
+    },
+    "2": {
+        "id": 2,
+        "pid": 0,
+        "title": "收费",
+        "checked":false
+    },
+    "3": {
+        "id": 3,
+        "pid": 2,
+        "title": "物业缴费",
+        "checked":false
+    },
+    "4": {
+        "id": 4,
+        "pid": 3,
+        "title": "水费",
+        "checked":false
+    },
+    "600": {
+        "id": 600,
+        "pid": 3,
+        "title": "电费",
+        "checked":false
+    },
+    "2999": {
+        "id": 2999,
+        "pid": 1,
+        "title": "身份证登记",
+        "checked":false
+    },
+    "4000": {
+        "id": 4000,
+        "pid": 3,
+        "title": "停车费",
+        "checked":false
+    },
+    "23333": {
+        "id": 23333,
+        "pid": 2,
+        "title": "物业欠费",
+        "checked":false
+    },
+    "233333": {
+        "id": 233333,
+        "pid": 23333,
+        "title": "查询客户",
+        "checked":false
+    },
+    "29000": {
+        "id": 29000,
+        "pid": 1,
+        "title": "刷脸登记",
+        "checked":false
+    },
+    "244444": {
+        "id": 244444,
+        "pid": 2,
+        "title": "权限管理",
+        "checked":false
+    },
+    "2444442": {
+        "id": 2444442,
+        "pid": 244444,
+        "title": "授权人员",
+        "checked":false
+    }
+}
+
